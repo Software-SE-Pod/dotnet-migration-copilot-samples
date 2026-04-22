e4fb1fd5cb87c02840a654c7cf67481b293656e5
Garrett London <glondon@microsoft.com>
2026-04-22 11:32:23 -0400

    migration(page contosouniversity-webforms-pages-departments-departmentedit): contract (openapi + nswag scaffolding) (#36)
    Co-authored-by: webforms-migration-bot <bot@users.noreply.github.com>

---
+++ dotnet/Api/Controllers/ContosouniversityWebformsPagesDepartmentsDepartmenteditController.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ApiContracts.Generated;
+
+namespace Api.Controllers
+{
+    public class ContosouniversityWebformsPagesDepartmentsDepartmenteditController : Generated.ContosouniversityWebformsPagesDepartmentsDepartmenteditControllerBase
+    {
+        public override IActionResult GetContosouniversityWebformsPagesDepartmentsDepartmentedit(int? id)
+        {
+            // TODO(coding-agent): port GetContosouniversityWebformsPagesDepartmentsDepartmentedit from ContosoUniversity.WebForms/Pages/Departments/DepartmentEdit.aspx.cs
+            throw new NotImplementedException();
+        }
+
+        public override IActionResult SubmitContosouniversityWebformsPagesDepartmentsDepartmentedit([FromBody] ContosouniversityWebformsPagesDepartmentsDepartmenteditSubmitRequest request)
+        {
+            // TODO(coding-agent): port SubmitContosouniversityWebformsPagesDepartmentsDepartmentedit from ContosoUniversity.WebForms/Pages/Departments/DepartmentEdit.aspx.cs
+            throw new NotImplementedException();
+        }
+    }
+}
